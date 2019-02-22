37fad03d30011a9d44984f1255cca21d39e50eba
clrnd <welcometothechango@gmail.com>
2019-02-22 09:58:55 -0300

    flattening type

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,22 +1,22 @@
 module Day4 where
 
-import           Data.Maybe            (fromJust)
-import           Data.Text            (Text)
-import qualified Data.Text            as Text
-import Data.Void
+import           Data.Maybe                     ( fromJust )
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as Text
+import           Data.Void
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
-import           Text.Megaparsec.Char.Lexer hiding (space)
+import           Text.Megaparsec.Char.Lexer
+                                         hiding ( space )
 
 type Parser = Parsec Void Text
 
-data LineType = Begins | Wakes | Falls
+data LineType = Begins Int | Wakes | Falls
   deriving (Eq, Ord, Show)
 
 data Line = Line
   { lineDay   :: String
   , lineTime  :: (Int, Int)
-  , lineGuard :: Maybe Int
   , lineType  :: LineType
   } deriving (Show, Eq)
 
@@ -24,16 +24,10 @@ parseLine :: Text -> Line
 parseLine = fromJust . parseMaybe parser
 
 parser :: Parser Line
-parser = do
-  date' <- char '[' *> date
-  time' <- time <* char ']'
-  space
-  (guard', type') <- guard <|> falls <|> wakes
-  many $ satisfy (const True)
-  pure $ Line date' time' guard' type'
-  where
-    date = some (alphaNumChar <|> char '-') <* space
-    time = (,) <$> decimal <*> (char ':' *> decimal)
-    guard = (,) <$> (Just <$> (string "Guard #" *> decimal)) <*> (pure Begins)
-    falls = (,) <$> (string "falls asleep" *> pure Nothing) <*> pure Falls
-    wakes = (,) <$> (string "wakes up" *> pure Nothing) <*> pure Wakes
+parser = Line <$> date <*> time <*> (guard <|> falls <|> wakes) <* space
+ where
+  date  = char '[' *> some (alphaNumChar <|> char '-') <* space
+  time  = (,) <$> decimal <*> (char ':' *> decimal <* char ']' <* space)
+  guard = string "Guard #" *> (Begins <$> decimal) <* " begins shift"
+  falls = string "falls asleep" *> pure Falls
+  wakes = string "wakes up" *> pure Wakes
--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -13,19 +13,16 @@ spec = do
       let line = "[1518-04-10 23:52] Guard #3559 begins shift"
       parseLine line === Line { lineDay="1518-04-10"
                               , lineTime=(23, 52)
-                              , lineGuard=Just 3559
-                              , lineType=Begins }
+                              , lineType=Begins 3559 }
 
     it "parses a falls" $ do
       let line = "[1518-04-10 23:52] falls asleep"
       parseLine line === Line { lineDay="1518-04-10"
                               , lineTime=(23, 52)
-                              , lineGuard=Nothing
                               , lineType=Falls }
 
     it "parses a wakes" $ do
       let line = "[1518-04-10 23:52] wakes up"
       parseLine line === Line { lineDay="1518-04-10"
                               , lineTime=(23, 52)
-                              , lineGuard=Nothing
                               , lineType=Wakes }
